78ad3ffb94d2c241a27722690cc29df574912d88
me-js-bro <me.js.bro@gmail.com>
2024-06-30 04:07:33 +0600

    finished

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,26 +20,26 @@ const Home = () => {
                 {/* Projects */}
                 <div className='lg:flex flex-col justify-center items-center my-10'>
 
-                    <h3 className='text-[26px] font-semibold'>Recent Activities</h3>
+                    <h3 className='text-[50px] font-semibold text-gray-700'>Recent Activities</h3>
 
                     <div className='lg:flex justify-center items-center my-10'>
                     <div className='flex flex-col justify-around items-center w-[500px] h-[500px] border-solid border-2 border-gray-200 rounded-md hover:shadow-2xl transition-shadow mx-5'>
                         <img src="https://lh3.googleusercontent.com/7CxCfH3CRtWGRRyDF1Nh86aw8JQapSrDV_ZEVZ4966VULxryLiN2wdRpaQMgn1JeG-73wJW-v8cxoiQom3gPDyDFqfYcgGEkwKn7-qwHFg5h3yV35S0TVYiYpCd5MXBmOg=w1280" alt="pitha" className='w-[400px]'/>
-                        <h3 className='text-[30px] font-semibold'>Pitha Utshab</h3>
+                        <h3 className='text-[30px] font-semibold text-gray-700'>Pitha Utshab</h3>
 
                         <Link to="/activities" className='bg-gray-200 text-gray-700 border-solid border-1 border-gray-400 hover:bg-gray-700 hover:border-gray-700 hover:text-white transition-all px-10 hover:px-20 hover:rounded-3xl py-3 rounded-2xl text-[20px] font-bold'>Read More</Link>
                     </div>
 
                     <div className='flex flex-col justify-around items-center w-[500px] h-[500px] border-solid border-2 border-gray-200 rounded-md hover:shadow-2xl transition-shadow mx-5'>
                         <img src="https://lh4.googleusercontent.com/NXGgC4d4ZRNzyunRV-MSzo9KRpP1_gdaxppctyOxyp-zx8ee687-sj85Pc-VR4AqPZDseaiEzha9BkW4eGBGn_-9BGIMOuOYT8Y-EFsU5JYpFL4tweAQtGXY2WhF6-mEcQ=w1280" alt="pitha" className='w-[400px]'/>
-                        <h3 className='text-[30px] font-semibold'>Annual Sports Day</h3>
+                        <h3 className='text-[30px] font-semibold text-gray-700'>Annual Sports Day</h3>
 
                         <Link to="/activities" className='bg-gray-200 text-gray-700 border-solid border-1 border-gray-400 hover:bg-gray-700 hover:border-gray-700 hover:text-white transition-all px-10 hover:px-20 hover:rounded-3xl py-3 rounded-2xl text-[20px] font-bold'>Read More</Link>
                     </div>
 
                     <div className='flex flex-col justify-around items-center w-[500px] h-[500px] border-solid border-2 border-gray-200 rounded-md hover:shadow-2xl transition-shadow mx-5'>
                         <img src="https://lh3.googleusercontent.com/0Ky5Uv8OnATGerzxNzHePkIXT999jgs4R-V4pnQVEsb4_doVBgwvFwtT7VbwxWyJ9_0Rsc92CishWSlf-QdqiNj4nB4DZPjrTawXtA6KHCD6XNpkkR-6ZX8pWPcsEg947A=w1280" alt="pitha" className='w-[400px]'/>
-                        <h3 className='text-[30px] font-semibold'>Celebrating Nobin Boron </h3>
+                        <h3 className='text-[30px] font-semibold text-gray-700'>Celebrating Nobin Boron </h3>
 
                         <Link to="/activities" className='bg-gray-200 text-gray-700 border-solid border-1 border-gray-400 hover:bg-gray-700 hover:border-gray-700 hover:text-white transition-all px-10 hover:px-20 hover:rounded-3xl py-3 rounded-2xl text-[20px] font-bold'>Read More</Link>
                     </div>
@@ -50,19 +50,19 @@ const Home = () => {
                 {/* Recent Projects */}
                 <div className='lg:flex flex-col justify-center items-center my-10'>
 
-                    <h3 className='text-[26px] font-semibold'>Our School Projects</h3>
+                    <h3 className='text-[50px] font-semibold text-gray-700'>Our School Projects</h3>
 
                     <div className='lg:flex justify-center items-center my-10'>
                     <div className='flex flex-col justify-around items-center w-[500px] h-[600px] border-solid border-2 border-gray-200 rounded-md hover:shadow-2xl transition-shadow mx-5'>
                         <img src="https://lh6.googleusercontent.com/WB9mu7N7NEMnTfRk51hB3g_0iI-LggeATt9r_HDbONWwBzTQTWBWati-SvLdkOurU6qJP9hmIir4IZrV-Jl6qhq8Uo8s7BVLW5kBoyXIGm0iK5SHwVpl91YdV3uAplt5Ug=w1280" alt="pitha" className='w-[400px]'/>
-                        <h3 className='text-[30px] font-semibold'>Robotics Project</h3>
+                        <h3 className='text-[30px] font-semibold text-gray-700'>Robotics Project</h3>
 
                         <Link to="/projects" className='bg-gray-200 text-gray-700 border-solid border-1 border-gray-400 hover:bg-gray-700 hover:border-gray-700 hover:text-white transition-all px-10 hover:px-20 hover:rounded-3xl py-3 rounded-2xl text-[20px] font-bold'>Read More</Link>
                     </div>
 
                     <div className='flex flex-col justify-around items-center w-[500px] h-[600px] border-solid border-2 border-gray-200 rounded-md hover:shadow-2xl transition-shadow mx-5'>
                         <img src="https://lh4.googleusercontent.com/Wk202JzrWLvKauKAURR7H4aw2jKGOceUaeQU-tixv-SE8GizW2gBrYGykiwUQeHeFIpcIvSlsUxQ04YNr29QGHUltzhP6TJtIhuQ1oBHerm_h_w_brDTA5IKgEtGrU18Rw=w1280" alt="pitha" className='w-[400px]'/>
-                        <h3 className='text-[30px] font-semibold'>Tree Plantation 
+                        <h3 className='text-[30px] font-semibold text-gray-700'>Tree Plantation 
                         </h3>
 
                         <Link to="/projects" className='bg-gray-200 text-gray-700 border-solid border-1 border-gray-400 hover:bg-gray-700 hover:border-gray-700 hover:text-white transition-all px-10 hover:px-20 hover:rounded-3xl py-3 rounded-2xl text-[20px] font-bold'>Read More</Link>
@@ -70,7 +70,7 @@ const Home = () => {
 
                     <div className='flex flex-col justify-around items-center w-[500px] h-[600px] border-solid border-2 border-gray-200 rounded-md hover:shadow-2xl transition-shadow mx-5'>
                         <img src="https://imgs.search.brave.com/woBq_cA2B0jj1Sx6SXqRqu8niRNtDkwZBkkRbAVo6Bg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9zb2xhci1wYW5l/bHMtcm9vZi1zb2xh/ci1jZWxsXzk1NDIy/Ni0yMDg3NzEuanBn/P3NpemU9NjI2JmV4/dD1qcGc" alt="pitha" className='w-[400px]'/>
-                        <h3 className='text-[30px] font-semibold'>Solar Panel</h3>
+                        <h3 className='text-[30px] font-semibold text-gray-700'>Solar Panel</h3>
 
                         <Link to="/projects" className='bg-gray-200 text-gray-700 border-solid border-1 border-gray-400 hover:bg-gray-700 hover:border-gray-700 hover:text-white transition-all px-10 hover:px-20 hover:rounded-3xl py-3 rounded-2xl text-[20px] font-bold'>Read More</Link>
                     </div>
@@ -82,12 +82,12 @@ const Home = () => {
                 {/* About Us */}
                 <div className='lg:flex flex-col justify-center items-center my-10'>
 
-                    <h3 className='text-[26px] font-semibold'>About Us</h3>
+                    <h3 className='text-[50px] font-semibold text-gray-700'>About Us</h3>
 
                     <div className='lg:flex justify-center items-center my-10'>
                     <div className='flex flex-col justify-around items-center w-[500px] h-[450px] border-solid border-2 border-gray-200 rounded-md hover:shadow-2xl transition-shadow mx-5'>
                         <img src={img4} alt="pitha" className='w-[400px]'/>
-                        <h3 className='text-[30px] font-semibold'>
+                        <h3 className='text-[30px] font-semibold text-gray-700'>
                             Name ?
                         </h3>
 
@@ -96,14 +96,14 @@ const Home = () => {
 
                     <div className='flex flex-col justify-around items-center w-[500px] h-[450px] border-solid border-2 border-gray-200 rounded-md hover:shadow-2xl transition-shadow mx-5'>
                         <img src={img3} alt="pitha" className='w-[400px]'/>
-                        <h3 className='text-[30px] font-semibold'>Name ? 
+                        <h3 className='text-[30px] font-semibold text-gray-700'>Name ? 
                         </h3>
                         <p>Marketing Directory</p>
                     </div>
 
                     <div className='flex flex-col justify-around items-center w-[500px] h-[450px] border-solid border-2 border-gray-200 rounded-md hover:shadow-2xl transition-shadow mx-5'>
                         <img src={img2} alt="pitha" className='w-[400px]'/>
-                        <h3 className='text-[30px] font-semibold'>Name ?</h3>
+                        <h3 className='text-[30px] font-semibold text-gray-700'>Name ?</h3>
                         <p>Editor & Content Creator</p>
                     </div>
                     </div>
--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='flex justify-center items-center my-3 text-gray-400'>
+            <h3> © 2024 School Bulletin- 9C Grp F All rights reserved </h3>
         </div>
     );
 };
